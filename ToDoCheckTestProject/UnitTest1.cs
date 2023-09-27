cbbfbd6999488bc3de985a9850832ef3a37f973f
samuel <samuelpayback@gmail.com>
2023-09-28 00:03:52 +0100

    Unit tests adapted to repository structure

--- ToDoCheckTestProject/UnitTest1.cs
+++ ToDoCheckTestProject/UnitTest1.cs
@@ -1,91 +1,78 @@
-﻿using NUnit.Framework;
-using toDoCheck.Services;
+﻿using toDoCheck.Services;
 using toDoCheck.Models;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+using toDoCheck.Repositories;
 
 namespace ToDoCheckTestProject;
 
 public class Tests
 {
-    private IToDoItemDB _toDoItemDBService;
-    private string _dbName;
+    private ToDoItemDBService<Foo> _DbService;
 
     [SetUp]
     public void Setup()
     {
-        // Generates a new dbName for each test
-        _dbName = $"Test_{Guid.NewGuid()}.db3";
-        _toDoItemDBService = new ToDoItemDBService(_dbName, true);
+        var inMemoryRepository = new InMemoryRepository<Foo>();
+        _DbService = new ToDoItemDBService<Foo>(inMemoryRepository);
     }
 
 
     [Test]
     public async Task InsertItemAsync_ShouldInsertItem()
     {
-        var item = new ToDoItem { ToDoTask = "Test Task" };
-        var insertResult = await _toDoItemDBService.InsertItemAsync(item);
+        var item = new Foo { Name = "Insert test" };
+        var insertResult = await _DbService.InsertItemAsync(item);
 
-        var retrievedItem = await _toDoItemDBService.GetItemsAsync();
+        var retrievedItem = await _DbService.GetItemsAsync();
         Assert.That(retrievedItem.Count, Is.EqualTo(1));
-        Assert.That(retrievedItem[0].ToDoTask, Is.EqualTo("Test Task"));
+        Assert.That(retrievedItem[0].Name, Is.EqualTo("Insert test"));
     }
 
     [Test]
     public async Task DeleteItemAsync_ShouldDeleteItem()
     {
-        var item = new ToDoItem { ToDoTask = "Test Task" };
-        var insertResult = await _toDoItemDBService.InsertItemAsync(item);
-        var deleteResult = await _toDoItemDBService.DeleteItemAsync(item);
+        var item = new Foo { Name = "Delte test" };
+        var insertResult = await _DbService.InsertItemAsync(item);
+        var deleteResult = await _DbService.DeleteItemAsync(item);
 
-        var retrievedItems = await _toDoItemDBService.GetItemsAsync();
+        var retrievedItems = await _DbService.GetItemsAsync();
         Assert.That(retrievedItems.Count, Is.EqualTo(0));
     }
 
     [Test]
-    public async Task ModifyItemAsync_ShouldUpdateItem()
+    public async Task UpdateItemAsync_ShouldUpdateItem()
     {
-        var item = new ToDoItem { ToDoTask = "Test Task" };
-        var insertResult = await _toDoItemDBService.InsertItemAsync(item);
+        var item = new Foo { Name = "Modify test" };
+        var insertResult = await _DbService.InsertItemAsync(item);
 
-        item.ToDoTask = "Modified Task";
-        var modifyResult = await _toDoItemDBService.ModifyItemAsync(item);
+        item.Name = "Modified test";
+        var modifyResult = await _DbService.UpdateItemAsync(item);
 
-        var retrievedItems = await _toDoItemDBService.GetItemsAsync();
-        Assert.That(retrievedItems[0].ToDoTask, Is.EqualTo("Modified Task"));
+        var retrievedItems = await _DbService.GetItemsAsync();
+        Assert.That(retrievedItems[0].Name, Is.EqualTo("Modified test"));
     }
 
     [Test]
-    public async Task InsertOrUpdateItemAsync_ShouldInsertNewItem()
+    public async Task GetAllAsync_ShouldUpdateItem()
     {
-        var item = new ToDoItem { ToDoTask = "Test Task" };
-        var insertUpdateResult = await _toDoItemDBService.InsertOrUpdateItemAsync(item);
+        for (int i = 0; i <= 2; i++)
+        {
+            var item = new Foo { Name = "GetAll test " + i };
+            var insertResult = await _DbService.InsertItemAsync(item);
+        }
+        
+        var retrievedItems = await _DbService.GetItemsAsync();
+        Assert.That(retrievedItems.Count, Is.EqualTo(3));
 
-        var retrievedItems = await _toDoItemDBService.GetItemsAsync();
-        Assert.That(retrievedItems.Count, Is.EqualTo(1));
+        for (int i = 0; i <= 2; i++)
+        {
+            Assert.That(retrievedItems[i].Name, Is.EqualTo("GetAll test " + i));
+        }
     }
 
-    [Test]
-    public async Task InsertOrUpdateItemAsync_ShouldUpdateExistingItem()
-    {
-        var item = new ToDoItem { ToDoTask = "Test Task" };
-        var insertResult = await _toDoItemDBService.InsertItemAsync(item);
-
-        item.ToDoTask = "Modified Task";
-        var insertUpdateResult = await _toDoItemDBService.InsertOrUpdateItemAsync(item);
-
-        var retrievedItems = await _toDoItemDBService.GetItemsAsync();
-        Assert.That(retrievedItems[0].ToDoTask, Is.EqualTo("Modified Task"));
-    }
 
     [TearDown]
     public void TearDown()
     {
-        var testDbPath = System.IO.Path.Combine(Environment.CurrentDirectory, _dbName); 
-
-        if (System.IO.File.Exists(testDbPath))
-        {
-            System.IO.File.Delete(testDbPath);
-        }
+        _DbService.Clear();
     }
 }
---
+++ toDoCheck/toDoCheck/Models/Foo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SQLite;
+
+namespace toDoCheck.Models
+{
+	public class Foo
+	{
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
+		public string Name { get; set; }
+    }
+}
