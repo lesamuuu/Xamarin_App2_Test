13d03f17cec21012b985fcc1ef27ca603745519c
samuel <samuelpayback@gmail.com>
2023-09-29 19:31:59 +0100

    Changed “Foo” class location from Main project to  TestProject

---
+++ ToDoCheckTestProject/Foo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SQLite;
+
+namespace ToDoCheckTestProject
+{
+    public class Foo
+    {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Foo otherItem)
+            {
+                return this.Id == otherItem.Id;
+            }
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
